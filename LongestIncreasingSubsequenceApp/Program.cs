48458b4bd0fab4d5cf76be557b6bdbfc100e3eac
Azeem Arif <mazeemarif0@gmail.com>
2024-08-08 14:35:40 +1000

    Remove obj and bin directories from the repository

---
+++ LongestIncreasingSubsequenceApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestIncreasingSubsequenceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var result = FindLongestIncreasingSubsequence(input);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        public static List<int> FindLongestIncreasingSubsequence(string input)
+        {
+            var numbers = input.Split(' ').Select(int.Parse).ToArray();
+            int n = numbers.Length;
+            if (n == 0) return new List<int>();
+
+            var longestSequence = new List<int>();
+            var currentSequence = new List<int> { numbers[0] };
+
+            for (int i = 1; i < n; i++)
+            {
+                if (numbers[i] > currentSequence.Last())
+                {
+                    currentSequence.Add(numbers[i]);
+                }
+                else
+                {
+                    if (currentSequence.Count > longestSequence.Count)
+                    {
+                        longestSequence = new List<int>(currentSequence);
+                    }
+                    currentSequence = new List<int> { numbers[i] };
+                }
+            }
+
+            if (currentSequence.Count > longestSequence.Count)
+            {
+                longestSequence = currentSequence;
+            }
+
+            return longestSequence;
+        }
+    }
+}
